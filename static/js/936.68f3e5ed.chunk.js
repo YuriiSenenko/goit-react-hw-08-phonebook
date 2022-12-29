48c10d9f7c67f31f8d42ba9adf638ec1166abe2d
YuriiSenenko <95052627+YuriiSenenko@users.noreply.github.com>
2022-12-29 16:52:55 +0000

    Deploying to gh-pages from @ YuriiSenenko/goit-react-hw-08-phonebook@72669c64a80c58c7314dd659cd0df6b8934fb635 🚀

---
+++ static/js/936.68f3e5ed.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{936:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n="Home_title__L087v",o=r(329),u=function(){return(0,o.jsx)("h1",{className:n,children:"This is your Phonebook"})}}}]);
+//# sourceMappingURL=936.68f3e5ed.chunk.js.map
