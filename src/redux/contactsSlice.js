ff88c4af69ed003efb6bc677ed8a21d37add7995
YuriiSenenko <yurkosenenko@gmail.com>
2023-01-05 22:24:05 +0200

    fixed delete contact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -42,8 +42,9 @@ export const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = null;
       const index = state.items.findIndex(
-        contact => contact.id === action.payload
+        contact => contact.id === action.payload.id
       );
+      console.log(index);
       state.items.splice(index, 1);
     },
   },
